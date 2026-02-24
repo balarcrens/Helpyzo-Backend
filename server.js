02c140cf441484fb7f1996e4df9489fa5c5503f3
balarcrens <balarcrens@gmail.com>
2026-02-24 15:33:36 +0530

    change api limit 20mb to 100mb

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(morganMiddleware);
 
 // API Routes
